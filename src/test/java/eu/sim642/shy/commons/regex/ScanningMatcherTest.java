0824bac002ab282c1134e91acc3ba80335ee3c81
Simmo Saan <simmo.saan@gmail.com>
2016-11-07 11:11:25 +0200

    ScanningMatcher: test hasNext, nextResult, next (#1)

---
+++ src/test/java/eu/sim642/shy/commons/regex/ScanningMatcherTest.java
@@ -0,0 +1,145 @@
+package eu.sim642.shy.commons.regex;
+
+import org.junit.Test;
+
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+
+import static org.junit.Assert.*;
+
+public class ScanningMatcherTest {
+
+    private final String DIGIT_PATTERN_STRING = "[0-9]+";
+    private final Pattern DIGIT_PATTERN = Pattern.compile(DIGIT_PATTERN_STRING);
+    private final String TWO_DIGIT_PATTERN_STRING = "([0-9])([0-9])";
+    private final Pattern TWO_DIGIT_PATTERN = Pattern.compile(TWO_DIGIT_PATTERN_STRING);
+    private final String ALPHA_PATTERN_STRING = "[a-z]+";
+    private final Pattern ALPHA_PATTERN = Pattern.compile(ALPHA_PATTERN_STRING);
+
+    @Test
+    public void testHasNextBegin() throws Exception {
+        ScanningMatcher matcher = new ScanningMatcher("123abc");
+
+        assertTrue(matcher.hasNext(DIGIT_PATTERN_STRING));
+        assertTrue(matcher.hasNext(DIGIT_PATTERN));
+        assertFalse(matcher.hasNext(ALPHA_PATTERN_STRING));
+        assertFalse(matcher.hasNext(ALPHA_PATTERN));
+    }
+
+    @Test
+    public void testHasNextMiddle() throws Exception {
+        ScanningMatcher matcher = new ScanningMatcher("123abc");
+        matcher.next(DIGIT_PATTERN);
+
+        assertFalse(matcher.hasNext(DIGIT_PATTERN_STRING));
+        assertFalse(matcher.hasNext(DIGIT_PATTERN));
+        assertTrue(matcher.hasNext(ALPHA_PATTERN_STRING));
+        assertTrue(matcher.hasNext(ALPHA_PATTERN));
+    }
+
+    @Test
+    public void testHasNextEnd() throws Exception {
+        ScanningMatcher matcher = new ScanningMatcher("123abc");
+        matcher.next(DIGIT_PATTERN);
+        matcher.next(ALPHA_PATTERN);
+
+        assertFalse(matcher.hasNext(DIGIT_PATTERN_STRING));
+        assertFalse(matcher.hasNext(DIGIT_PATTERN));
+        assertFalse(matcher.hasNext(ALPHA_PATTERN_STRING));
+        assertFalse(matcher.hasNext(ALPHA_PATTERN));
+    }
+
+    @Test
+    public void testNextResultSuccess() throws Exception {
+        ScanningMatcher matcher = new ScanningMatcher("123abc");
+
+        MatchResult result123 = matcher.nextResult(DIGIT_PATTERN);
+        assertEquals(0, result123.start());
+        assertEquals(3, result123.end());
+        assertEquals("123", result123.group());
+
+        MatchResult resultabc = matcher.nextResult(ALPHA_PATTERN);
+        assertEquals(3, resultabc.start());
+        assertEquals(6, resultabc.end());
+        assertEquals("abc", resultabc.group());
+    }
+
+    @Test
+    public void testNextResultFailureBegin() throws Exception {
+        ScanningMatcher matcher = new ScanningMatcher("123abc");
+
+        assertNull(matcher.nextResult(ALPHA_PATTERN));
+
+        MatchResult result123 = matcher.nextResult(DIGIT_PATTERN);
+        assertEquals(0, result123.start());
+        assertEquals(3, result123.end());
+        assertEquals("123", result123.group());
+    }
+
+    @Test
+    public void testNextResultFailureMiddle() throws Exception {
+        ScanningMatcher matcher = new ScanningMatcher("123abc");
+        matcher.next(DIGIT_PATTERN);
+
+        assertNull(matcher.nextResult(DIGIT_PATTERN));
+
+        MatchResult resultabc = matcher.nextResult(ALPHA_PATTERN);
+        assertEquals(3, resultabc.start());
+        assertEquals(6, resultabc.end());
+        assertEquals("abc", resultabc.group());
+    }
+
+    @Test
+    public void testNextResultFailureEnd() throws Exception {
+        ScanningMatcher matcher = new ScanningMatcher("123abc");
+        matcher.next(DIGIT_PATTERN);
+        matcher.next(ALPHA_PATTERN);
+
+        assertNull(matcher.nextResult(DIGIT_PATTERN));
+        assertNull(matcher.nextResult(ALPHA_PATTERN));
+    }
+
+    @Test
+    public void testNextResultGroups() throws Exception {
+        ScanningMatcher matcher = new ScanningMatcher("123abc");
+
+        MatchResult result12 = matcher.nextResult(TWO_DIGIT_PATTERN);
+        assertEquals("1", result12.group(1));
+        assertEquals("2", result12.group(2));
+    }
+
+    @Test
+    public void testNextSuccess() throws Exception {
+        ScanningMatcher matcher = new ScanningMatcher("123abc");
+
+        assertEquals("123", matcher.next(DIGIT_PATTERN));
+        assertEquals("abc", matcher.next(ALPHA_PATTERN));
+    }
+
+    @Test
+    public void testNextFailureBegin() throws Exception {
+        ScanningMatcher matcher = new ScanningMatcher("123abc");
+
+        assertNull(matcher.next(ALPHA_PATTERN));
+        assertEquals("123", matcher.next(DIGIT_PATTERN));
+    }
+
+    @Test
+    public void testNextFailureMiddle() throws Exception {
+        ScanningMatcher matcher = new ScanningMatcher("123abc");
+        matcher.next(DIGIT_PATTERN);
+
+        assertNull(matcher.next(DIGIT_PATTERN));
+        assertEquals("abc", matcher.next(ALPHA_PATTERN));
+    }
+
+    @Test
+    public void testNextFailureEnd() throws Exception {
+        ScanningMatcher matcher = new ScanningMatcher("123abc");
+        matcher.next(DIGIT_PATTERN);
+        matcher.next(ALPHA_PATTERN);
+
+        assertNull(matcher.next(DIGIT_PATTERN));
+        assertNull(matcher.next(ALPHA_PATTERN));
+    }
+}
